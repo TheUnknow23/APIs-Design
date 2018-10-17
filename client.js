386af97941e5359d3405c93062a9f5f0fa65ab42
TheUnknow23 <32764086+TheUnknow23@users.noreply.github.com>
2018-10-17 09:47:50 +0200

    First

---
+++ client.js
@@ -0,0 +1,16 @@
+const fetch = require("node-fetch");
+const url = "localhost:3000/courses"
+
+const getLocation = async url => {
+
+    try {
+        const response = await fetch(url);
+        const json = await response.json();
+        console.log(json)
+    } catch (error) {
+        console.log(error);
+    }
+};
+
+
+getLocation(url);
